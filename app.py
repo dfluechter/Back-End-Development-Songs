eee39e14509d4a80fecdc6b8842e91c144588fad
dfluechter <dominik.fluechter@googlemail.com>
2024-04-10 03:59:26 -0400

    on task 2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify, request
-from db_init import collection
+from flask import Flask
+, jsonify, request
+import db.py
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ def get_songs():
 @app.route("/song", methods=["POST"])
 def create_song():
     song = request.get_json()
-    collection.insert_one(song)
+    songs.insert_one(song)
     return jsonify(song), 201
 
 @app.route("/song/<id>", methods=["GET"])
@@ -21,13 +22,13 @@ def get_song(id):
 
 @app.route("/song/<id>", methods=["PUT"])
 def update_song(id):
-    song = request.get_json()
+    songs = request.get_json()
     collection.update_one({"_id": id}, {"$set": song})
     return jsonify(song)
 
 @app.route("/song/<id>", methods=["DELETE"])
 def delete_song(id):
-    collection.delete_one({"_id": id})
+    songs.delete_one({"_id": id})
     return "", 204
 
 @app.route("/health", methods=["GET"])
@@ -36,4 +37,4 @@ def health():
 
 @app.route("/count", methods=["GET"])
 def count():
-    return jsonify({"count": collection.count_documents({})})
+    return jsonify({"count": songs.count_documents({})})
